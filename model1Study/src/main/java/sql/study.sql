a6e27d13c300b48fb8f33a3d6f3683224d7e2744
kmsclass <kms@goodee.co.kr>
2025-04-04 16:05:05 +0900

    '20250404-0405'

---
+++ model1Study/src/main/java/sql/study.sql
@@ -0,0 +1,12 @@
+-- /java/sql/study.sql
+create table member (
+   id varchar(20) primary key,
+   pass varchar(20),
+   name varchar(20),
+   gender int(1),
+   tel varchar(15),
+   email varchar(50),
+   picture varchar(200)
+)
+
+select * from member
