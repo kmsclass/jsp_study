80a87c85b54f161fd7db7faa01ce30962ea8d0e9
kmsclass <kms@goodee.co.kr>
2025-04-15 15:52:24 +0900

    '20250415-1552'

---
+++ mybatisstudy/src/test0415/Test1.java
@@ -0,0 +1,18 @@
+package test0415;
+/*
+ * StudentMapper1.xml 파일을 이용하기
+ * 1. 학생테이블의 등록된 레코드의 건수를 출력하기
+ * 2. 학생테이블의 등록된 레코드의 정보를 출력하기
+ * 3. 학생테이블의 등록된 레코드의 1학년 학생의 정보를 출력하기
+ * 4. 학생테이블의 등록된 레코드의 성이 김씨인 학생의 정보를 출력하기
+ * 5. 학생테이블의 등록된 레코드의 3학년 학생 중 주민번호 기준 여학생 정보를 출력하기
+ */
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
